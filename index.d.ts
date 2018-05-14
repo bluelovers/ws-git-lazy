ca7a7103da32e08fd0380d6732792975104835a3
bluelovers <codelovers@users.sourceforge.net>
2018-05-15 05:35:52 +0800

    .

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 2018/5/15/015.
+ */
+export declare function encode(s: string): string;
+export declare function decode(s: any): string;
+import * as self from './index';
+export default self;
---
+++ test/demo.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 2018/5/15/015.
+ */
+
+import { decode, encode } from '..';
+
+console.log(decode('\\351\\255\\224\\347\\245\\236\\350\\273\\242\\347\\224\\2372.CT'));
+console.log(encode('魔神転生2.CT'));
+
+
+
