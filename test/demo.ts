6cf1d92b39efe0e23e74ee9703df4e4dfc7f11c9
bluelovers <codelovers@users.sourceforge.net>
2018-05-27 10:19:25 +0800

    [bugfix][breaking change] 修正在取得真實 rev id 時 沒有包含起始點的問題

--- test/demo.ts
+++ test/demo.ts
@@ -5,8 +5,11 @@
 import * as path from "path";
 import gitDiffFrom from '../index';
 
-let r = gitDiffFrom(10, {
-	cwd: path.resolve('D:/Users/Documents/The Project/nodejs-test/node-novel2/dist_novel'),
+let from = '96d60cdc0';
+
+let r = gitDiffFrom(from, 'origin/master', {
+	cwd: path.resolve('D:/Users/Documents/The Project/nodejs-yarn/ws-novel/packages/node-novel-travis-test/dist_novel'),
+
 });
 
 console.log(r);
